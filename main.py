5feeadcbafbd6c401f58ece2019cac2ede2343a8
sam tiso <samtiso@Sammys-computer.local>
2022-03-29 19:03:14 -0400

    working score tracker implemented, reworked snake and mouse to rat and cheese

--- main.py
+++ main.py
@@ -10,50 +10,50 @@ size = 45
 
 class Food:
     def __init__(self, main_screen):
-        mouse = pygame.image.load("img/pixel_mouse.gif").convert()
-        default_mouse_size = (60, 80)
-        self.image = pygame.transform.scale(mouse, default_mouse_size)
+        cheese = pygame.image.load("img/chez.png").convert_alpha()
+        default_cheese_size = (40, 40)
+        self.image = pygame.transform.scale(cheese, default_cheese_size)
         self.main_screen = main_screen
-        self.mouse_x = size * 5
-        self.mouse_y = size * 5
+        self.cheese_x = size * 3
+        self.cheese_y = size * 3
 
-    def draw_mouse(self):
-        self.main_screen.blit(self.image, (self.mouse_x, self.mouse_y))
+    def draw_cheese(self):
+        self.main_screen.blit(self.image, (self.cheese_x, self.cheese_y))
         pygame.display.flip()
-# method to create a new mouse in a new position when previous mouse is collided with
+# method to create new cheese in a new position when previous cheese is collided with
     def new_food(self):
-        self.mouse_x = random.randint(0, 20) * size
-        self.mouse_y = random.randint(0, 20) * size
+        self.cheese_x = random.randint(0, 20) * size
+        self.cheese_y = random.randint(0, 20) * size
 
-class Snake:
+class Rat:
     def __init__(self, main_screen, length):
         self.main_screen = main_screen
         self.length = length
 
-    # importing image as snake head and resizing it
-        face = pygame.image.load("img/snake_head_block.png").convert()
-        default_head_size = (size, size)
-        self.head = pygame.transform.scale(face, default_head_size)
+    # importing image and resizing it
+        image = pygame.image.load("img/pixel_mouse.gif").convert_alpha()
+        default_image_size = (size*2, size*2)
+        self.rat = pygame.transform.scale(image, default_image_size)
 
-    # setting the default position for the head and rendering it on the surface, using array to hold subsequent blocks accumulated
-        self.head_x = [size] * length
-        self.head_y = [size] * length
+    # setting the default position for the rat and rendering it on the surface, using array to hold subsequent blocks accumulated
+        self.rat_x = [size] * length
+        self.rat_y = [size] * length
 
-    # creating a direction for the snake to move continuously
+    # creating a direction for the rat to move continuously
         self.direction = 'down'
 
-# method to render head responsive to movement
-    def draw_head(self):
+# method to render rat responsive to movement
+    def draw_rat(self):
         self.main_screen.fill((194, 178, 128)) 
         for i in range(self.length):
-            self.main_screen.blit(self.head, (self.head_x[i], self.head_y[i]))
+            self.main_screen.blit(self.rat, (self.rat_x[i], self.rat_y[i]))
         pygame.display.flip()
 
-# method to increase size of snake when a mouse is eaten (adding to end of array)
+# method to increase number of rats when cheese is eaten (adding to end of array)
     def grow(self):
         self.length += 1
-        self.head_x.append(-1)
-        self.head_y.append(-1)
+        self.rat_x.append(-1)
+        self.rat_y.append(-1)
 
 # functions to move i a direction based on keystrokes
     def move_left(self):
@@ -70,21 +70,21 @@ class Snake:
 
 
     def walk(self):
-    # Move accumulated blocks to position of block in front of it (current block position is previous blocks position)
+    # Move accumulated rats to position in front of it (current blocks old position is previous blocks new position)
         time.sleep(0.2)
         for i in range(self.length-1, 0, -1):
-            self.head_x[i] = self.head_x[i-1]
-            self.head_y[i] = self.head_y[i-1]
-    # move snake continuously based on direction one full block size on each movement
+            self.rat_x[i] = self.rat_x[i-1]
+            self.rat_y[i] = self.rat_y[i-1]
+    # move rat continuously based on direction one full block size on each movement
         if self.direction == 'up':
-            self.head_y[0] -= size
+            self.rat_y[0] -= size
         if self.direction == 'down':
-            self.head_y[0] += size
+            self.rat_y[0] += size
         if self.direction == 'left':
-            self.head_x[0] -= size
+            self.rat_x[0] -= size
         if self.direction == 'right':
-            self.head_x[0] += size
-        self.draw_head()
+            self.rat_x[0] += size
+        self.draw_rat()
         
 class Game:
     def __init__(self):
@@ -95,33 +95,42 @@ class Game:
         self.surface = pygame.display.set_mode((1000, 1000))
         self.surface.fill((194, 178, 128))
 
-    # creating the snake inside of the game by using the snake class (expects main_screen value)
-        self.snake = Snake(self.surface, 1)
-        self.snake.draw_head()
+    # creating the rat inside of the game by using the Rat class (expects main_screen value)
+        self.rat = Rat(self.surface, 1)
+        self.rat.draw_rat()
 
-    # initializing the mouse inside of the game
+    # initializing the cheese inside of the game
         self.food = Food(self.surface)
-        self.food.draw_mouse()
+        self.food.draw_cheese()
 
 
     def collide(self, x1, y1, x2, y2):
-        # if the coordinates of snake are within the coordinates of mouse, collision is true 
-        if x1 >= x2 and x1 <= x2 + size:
-            if y1 >= y2 and y1 <= y2 + size:
+        # if the coordinates of rat are within the coordinates of cheese, collision is true 
+        if x1 >= x2 and x1 < x2 + size:
+            if y1 >= y2 and y1 < y2 + size:
                 return True
         return False
 
 
     def play(self):
-    # rendering snake walk
-        self.snake.walk()
+    # rendering rat walk
+        self.rat.walk()
     # to ensure that when screen is rendered, food isn't wiped off
-        self.food.draw_mouse() 
-    # passing snake coordinates and mouse coordinates as x/y 1 and 2 values from collide method
-        if self.collide(self.snake.head_x[0], self.snake.head_y[0], self.food.mouse_x, self.food.mouse_y):
+        self.food.draw_cheese() 
+
+        self.keep_score()
+        pygame.display.flip()
+    # passing rat coordinates and cheese coordinates as x/y 1 and 2 values from collide method
+        if self.collide(self.rat.rat_x[0], self.rat.rat_y[0], self.food.cheese_x, self.food.cheese_y):
             self.food.new_food()
-        # when snake collides, increase length and add a block to the array
-            self.snake.grow()
+        # when rat collides, increase length and add a block to the array
+            self.rat.grow()
+
+
+    def keep_score(self):
+        font = pygame.font.SysFont('arial', 40)
+        score = font.render(f"{self.rat.length -1}", True, (0, 0, 0))
+        self.surface.blit(score, (950, 15))
 
 # setting game up to run and giving keystrokes functionality        
     def run_game(self):
@@ -132,19 +141,19 @@ class Game:
                     if event.key == K_ESCAPE:
                         running = False
                     if event.key == K_UP:
-                        self.snake.move_up()
+                        self.rat.move_up()
                         # head_y -= 20
                         # draw_head()
                     if event.key == K_DOWN:
-                        self.snake.move_down()
+                        self.rat.move_down()
                         # head_y += 20
                         # draw_head()
                     if event.key == K_LEFT:
-                        self.snake.move_left()
+                        self.rat.move_left()
                         # head_x -= 20
                         # draw_head()
                     if event.key == K_RIGHT:
-                        self.snake.move_right()
+                        self.rat.move_right()
                         # head_x += 20
                         # draw_head()
                 elif event.type == QUIT:
