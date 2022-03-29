54be1b07e9aa9be928ca721d73e7136145e52179
sam tiso <samtiso@Sammys-computer.local>
2022-03-29 13:20:51 -0400

    multiple block aka snake tail added, currently hard coded

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class Game:
         self.surface.fill((194, 178, 128))
 
     # creating the snake inside of the game by using the snake class (expects main_screen value)
-        self.snake = Snake(self.surface)
+        self.snake = Snake(self.surface, 8)
         self.snake.draw_head()
 
 # setting game up to run and giving keystrokes functionality        
@@ -46,18 +46,21 @@ class Game:
             self.snake.walk()
             time.sleep(0.1)
 
+size = 25
+
 class Snake:
-    def __init__(self, main_screen):
+    def __init__(self, main_screen, length):
         self.main_screen = main_screen
+        self.length = length
 
     # importing image as snake head and resizing it
         face = pygame.image.load("img/snake_head_block.png").convert()
         default_head_size = (25, 25)
         self.head = pygame.transform.scale(face, default_head_size)
 
-    # setting the default position for the head and rendering it on the surface
-        self.head_x = 500
-        self.head_y = 500
+    # setting the default position for the head and rendering it on the surface, using array to hold subsequent blocks accumulated
+        self.head_x = [size] * length
+        self.head_y = [size] * length
 
     # creating a direction for the snake to move continuously
         self.direction = 'down'
@@ -65,7 +68,8 @@ class Snake:
 # function to render head according to keystroke
     def draw_head(self):
         self.main_screen.fill((194, 178, 128)) 
-        self.main_screen.blit(self.head, (self.head_x, self.head_y))
+        for i in range(self.length):
+            self.main_screen.blit(self.head, (self.head_x[i], self.head_y[i]))
         pygame.display.flip()
 
 # functions to move i a direction based on keystrokes
@@ -81,16 +85,21 @@ class Snake:
     def move_down(self):
         self.direction = 'down'
 
-# walk function to move snake continuously based on direction
+
     def walk(self):
+    # Move accumulated blocks to position of block in front of it (current block position is previous blocks position)
+        for i in range(self.length-1, 0, -1):
+            self.head_x[i] = self.head_x[i-1]
+            self.head_y[i] = self.head_y[i-1]
+    # move snake continuously based on direction one full block size on each movement
         if self.direction == 'up':
-            self.head_y -= 20
+            self.head_y[0] -= size
         if self.direction == 'down':
-            self.head_y += 20
+            self.head_y[0] += size
         if self.direction == 'left':
-            self.head_x -= 20
+            self.head_x[0] -= size
         if self.direction == 'right':
-            self.head_x += 20
+            self.head_x[0] += size
 
         self.draw_head()
 
